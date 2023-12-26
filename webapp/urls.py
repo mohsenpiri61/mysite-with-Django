c4e39b082dc706bf542de8de3e457386b67a7369
mohsenpiri61 <mopiry@gmail.com>
2023-12-26 11:43:34 +0330

    creation template
    Template for project

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,5 +4,6 @@ from webapp.views import *
 urlpatterns = [
     path('home', home_view),
     path('about', about_text),
-    path('contact', contact_list)
+    path('contact', contact_list),
+    path('index', index_show)
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 def home_view(request):
     return HttpResponse('<h1> You are in Home page </h1>')
 
@@ -7,3 +8,6 @@ def about_text(request):
 
 def contact_list(request):
     return JsonResponse({'name': 'mohsen', 'age': 41})
+
+def index_show(request): # refer to templates folder
+    return render(request, 'index.html')
