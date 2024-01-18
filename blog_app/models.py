1ad04a3eabaf69009685f996f05c080afe49571d
mohsenpiri61 <mopiry@gmail.com>
2024-01-18 11:10:00 +0330

    for exam3

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,14 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='app_image/', default='default.png')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     counted_views = models.PositiveIntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    category_list = models.ManyToManyField(Category)
 
     class Meta:
         ordering = ['-created_date']
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,19 +3,35 @@ from blog_app.models import Post
 from django.utils import timezone
 
 
-def home_view(request):  
+def home_view(request):
     # To display posts whose publication date is before the current day and status=1#
     filter_post = Post.objects.filter(published_date__lt=timezone.now(), status=1)
-    #filter_post = Post.objects.filter(status=1)  # posts that only have status=1 
-    context = {'post_filtered': filter_post}
+    #filter_post = Post.objects.filter(status=1)  # posts that only have status=1
+    context = {'filter_post': filter_post}
     return render(request, 'blog_items/blog-home.html', context)
 
 
 def single_view(request, pid):  # refer to blog_items folder in template folder
-    post_id = get_object_or_404(Post, id=pid, status=1, published_date__lt=timezone.now())
-    post_id.counted_views = post_id.counted_views + 1
-    post_id.save()
-    context = {'post_obj': post_id}
+    post_obj = get_object_or_404(Post, id=pid, status=1, published_date__lt=timezone.now())
+    post_obj.counted_views = post_obj.counted_views + 1
+    post_obj.save()
+    filter_post = Post.objects.filter(published_date__lt=timezone.now(), status=1)
+    post_ids = [post.id for post in filter_post]
+    pid_index = post_ids.index(pid)
+    if pid_index == 0:
+        next_id = post_ids[pid_index+1]
+        next_obj = Post.objects.get(id=next_id)
+        prev_obj = Post.objects.none()
+    elif pid_index == post_ids.index(len(post_ids)-1):
+        prev_id = post_ids[pid_index-1]
+        prev_obj = Post.objects.get(id=prev_id)
+        next_obj = Post.objects.none()
+    else:
+        next_id = post_ids[pid_index+1]
+        next_obj = Post.objects.get(id=next_id)
+        prev_id = post_ids[pid_index - 1]
+        prev_obj = Post.objects.get(id=prev_id)
+    context = {'post_obj': post_obj, 'next_obj': next_obj, 'prev_obj': prev_obj}
     return render(request, 'blog_items/blog-single.html', context)
 
 def fetch_view(request):  # refer to templates folder
