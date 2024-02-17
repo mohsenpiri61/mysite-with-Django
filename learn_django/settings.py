54e38c5ef5f87533d8599f99882b8581a20b0731
mohsenpiri61 <mopiry@gmail.com>
2024-02-17 19:23:59 +0330

    update requirements.txt

--- learn_django/settings.py
+++ learn_django/settings.py
@@ -171,7 +171,7 @@ INTERNAL_IPS = [
 
     "127.0.0.1",
 ]
-
+'''
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER =   # your email-id
@@ -179,3 +179,4 @@ EMAIL_HOST_PASSWORD =   # your password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+'''
