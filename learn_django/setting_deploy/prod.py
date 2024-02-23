842055aac3ea933c8a4aa64c088f5a73b14f2eca
mohsenpiri61 <mopiry@gmail.com>
2024-02-23 21:58:09 +0330

    deploy2

--- learn_django/setting_deploy/prod.py
+++ learn_django/setting_deploy/prod.py
@@ -7,12 +7,20 @@ from learn_django.settings import *
 SECRET_KEY = 'django-insecure-om614tz48)mby5eiq*8g@u7=+=faz)w^mw0*q5t+m@yq8=w878'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mohsen-piri.com', 'www.mohsen-piri.com']
 
 # site framework
 SITE_ID = 4
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
